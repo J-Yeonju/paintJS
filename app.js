b7f6f16e607af605b82686e665b8d220d741d2bd
l0o0tus <l0o0tus@naver.com>
2022-02-03 16:27:48 +0900

    commit

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
 
 let painting = false;
 
@@ -6,23 +11,30 @@ function stopPainting(){
     painting = false;
 }
 
-function onMouseMove(event) {
-    const x = event.offsetX;
-    const y = event.offsetY;
-}
-function onMouseDown(event){
+function startPainting(){
     painting = true;
 }
 
-function onMouseUp(event){
-    stopPainting();
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+function onMouseDown(event){
+    painting = true;
 }
 
 
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", startPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
